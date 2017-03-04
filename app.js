9dfc3cb7fe62a20d5c72fdc3cf6b43ce0ef86df9
ammiel <aemlcruz@gmail.com>
2017-03-03 21:13:06 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,67 @@
+var express = require("express"),
+bodyParser = require("body-parser"),
+mongoose = require("mongoose"),
+app = express();
+
+// app config
+mongoose.connect("mongodb://localhost/restful_blog_app");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+// mongoose/model config
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+// resful routes
+app.get("/", function(req, res) {
+    res.redirect("/blogs");
+});
+
+// index route
+app.get("/blogs", function(req, res) {
+    Blog.find({}, function(err, blogs) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render("index", {blogs: blogs});
+        }
+    });
+});
+
+// new route
+app.get("/blogs/new", function(req, res) {
+    res.render("new");
+});
+
+// create route
+app.post("/blogs", function(req, res) {
+    Blog.create(req.body.blog, function(err, newBlog) {
+        if(err) {
+            res.render("new");
+        } else {
+            res.redirect("/blogs");
+        }
+    });
+});
+
+// show route
+app.get("/blogs/:id", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog) {
+        if(err) {
+            res.redirect("/blogs");
+        } else {
+            res.render("show", {blog: foundBlog});
+        }
+    })
+});
+
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("Server is running!");
+});
